aba4d1ef469f833e789127a382ba28623001d805
eliasadmaso1 <76824469+eliasadmaso1@users.noreply.github.com>
2021-04-01 14:44:20 +0300

    api project files

---
+++ main.js
@@ -0,0 +1,141 @@
+function findApi(){
+   return fetch("https://next.json-generator.com/api/json/get/NJ-UoW2Xq")
+.then((response) => {
+return response.json(); })
+}
+
+function goToUserPage(userId){
+  window.location.replace(`user-page/user-page.html?id=${userId}`)
+}
+
+
+let results = [];
+
+async function onBody(){
+    try{
+        
+        results = await findApi();
+        results.forEach(element => {
+            mainDiv.innerHTML += `
+        
+          <div class="card" onclick="goToUserPage('${element._id}')" >
+          <h2>First Name - ${element.name.first}</h2>
+          <h2>Last Name - ${element.name.last}</h2>
+          <h2>Email - ${element.email}</h2>
+          <h2>Age - ${element.age}</h2>
+          <h2>Phone - ${element.phone}</h2>
+          </div>
+         
+          `});
+          
+
+    }
+    catch{
+        mainDiv.innerHTML = `not found`; 
+    }
+   }
+   onBody()
+
+
+
+let objApi2 = [];
+async function tableApi(){
+    
+   try{
+        objApi2 = await findApi();
+        document.body.innerHTML = `
+        <table style="width:100%">
+        <tr>
+          <th style="width:300px">Firstname</th>
+          <th style="width:300px">Lastname</th>
+          <th style="width:300px">Email</th>
+          <th style="width:300px">Age</th>
+          <th style="width:300px">Phone</th>
+        </tr>
+        </table>
+        `
+        objApi2.forEach(element => {
+          document.body.innerHTML +=
+            `
+            <div style="background: firebrick;">
+            <table style="width:100%">
+            <tr>
+              <td >${element.name.first}</td>
+              <td>${element.name.last}</td>
+              <td>${element.email}</td>
+              <td>${element.age}</td>
+              <td>${element.phone}</td>
+            </tr>
+            </table>
+           </div>
+          `}) 
+          document.body.innerHTML += `<button onclick="goToHome()">Return</button>`      
+          
+
+    }
+    catch{
+        mainDiv.innerHTML = `not found`; 
+    }
+   } 
+   function goToHome() {
+    window.location.replace("../index.html");
+  }
+
+   
+    const options = {
+      method:'POST',
+      body:JSON.stringify({
+       age: 25,
+       email: "dollie.hensley@undefined.il",
+       index: 10,
+       name: {first: "Elias", last: "Admaso"},
+       phone: "+1 (904) 421-31",
+       picture: "http://placehold.it/32",
+       _id: "605acace4ab389d8ed54c"
+      })
+    }
+
+  function postToApi(){
+    return fetch("https://next.json-generator.com/api/json/get/NJ-UoW2Xq", options)
+    .then((response) => {
+   return response.json(); 
+   
+  }) 
+  }
+
+let postObject = [];
+async function addUser(){
+    try{
+        
+        postObject = await postToApi();
+        console.log(postObject);
+        
+   
+          
+       
+         
+          
+          alert("user added");
+          
+
+    }
+    catch{
+        mainDiv.innerHTML = `not found`; 
+    }
+   }
+
+   
+
+  
+
+   
+
+
+   
+   
+
+
+
+
+   
+
---
+++ user-page/user-page.js
@@ -0,0 +1,62 @@
+function findApi() {
+  return fetch("https://next.json-generator.com/api/json/get/NJ-UoW2Xq").then(
+    (response) => {
+      return response.json();
+    }
+  );
+}
+
+let results = [];
+async function toUserPage() {
+  try {
+    results = await findApi();
+
+// window.location.search = ?id=605acace4ab389d8ed54c496&firstName=daniel&email=asdas@gmail.com
+
+/* 
+URLSearchParams under the hook create a Map and set all the keys and values of the query params:
+const queryParams = new Map(); // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map/set
+queryParams.set('id', '605acace4ab389d8ed54c496');
+queryParams.set('firstName', 'daniel');
+queryParams.set('email', 'asdas@gmail.com');
+
+Then you can get the value of the key you want:
+ 
+const userId = queryParams.get("id");
+console.log(userId)
+*/
+
+
+
+
+    const queryParams = new URLSearchParams(window.location.search); // window.location.search = all the string after the ?
+
+    const userId = queryParams.get("id");
+
+    console.log({results, userId})
+    results.forEach((element) => {
+      if (userId == element._id) {
+        document.body.innerHTML = 
+               `<h1>User Page</h1>
+                <div class="card">
+                <h2>First Name - ${element.name.first}</h2>
+                <h2>Last Name - ${element.name.last}</h2>
+                <h2>email - ${element.email}</h2>
+                <h2>Age - ${element.age}</h2>
+                <h2>Phone - ${element.phone}</h2>
+
+               </div>
+               <button onclick="goToHome()"> Back to home </button>
+               `;
+      }
+    });
+  } catch {
+    mainDiv.innerHTML = `not found`;
+  }
+}
+
+toUserPage();
+
+function goToHome() {
+  window.location.replace("../index.html");
+}
